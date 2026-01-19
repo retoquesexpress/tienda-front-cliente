ef49cccfda1a04ef7593e850d1a46a6f9243fed0
Carlos S <carlossaw22@gmail.com>
2026-01-19 12:15:26 +0100

    feat(retoquesexpress/sprint3#58): Componente Bloque Imagenes

---
+++ src/app/components/pages/tienda/unas/unas.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CBloque3Imagenes } from '../../../ui/c-bloque-3-imagenes/c-bloque-3-imagenes';
+
+@Component({
+    selector: 'app-unas',
+    standalone: true,
+    imports: [CBloque3Imagenes],
+    templateUrl: './unas.html',
+    styleUrl: './unas.scss'
+})
+export class Unas {
+
+
+}
---
+++ src/app/components/ui/c-bloque-3-imagenes/c-bloque-3-imagenes.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-c-bloque-3-imagenes',
+  imports: [],
+  templateUrl: './c-bloque-3-imagenes.html',
+  styleUrl: './c-bloque-3-imagenes.scss'
+})
+export class CBloque3Imagenes {
+}
